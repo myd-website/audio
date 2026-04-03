6bcc75259c8cfe0602ec64a0b7633b39f66b338a
mayandong <yandong.ma@77hub.com>
2026-04-03 18:29:05 +0800

    版本更新

--- docs/static/js/use-expose.js
+++ docs/static/js/use-expose.js
@@ -1,1 +1,1 @@
-import{K as t,B as s}from"./index.js";function r(n){const e=s();e&&t(e.proxy,n)}export{r as u};
+import{T as t,B as s}from"./index.js";function r(n){const e=s();e&&t(e.proxy,n)}export{r as u};
